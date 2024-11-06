fd0283e5547f3dfb5891eb73b173a6341493dec4
umar-tung <umartung@gmail.com>
2024-11-06 00:48:29 -0800

    Allow multiple messages from the client

--- server/Server.cs
+++ server/Server.cs
@@ -6,12 +6,13 @@ using System.Threading.Tasks;
 using System.Collections.Concurrent;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
+using System.Diagnostics;
 
 class Server
 {
     static void Main()
     {
-        ConcurrentDictionary<TcpClient, Player> players = new ConcurrentDictionary<TcpClient, Player>(); 
+        ConcurrentDictionary<TcpClient, Player> players = new ConcurrentDictionary<TcpClient, Player>();
         IPAddress ip = IPAddress.Parse("127.0.0.1");
         TcpListener server = new TcpListener(ip, 8080);
 
@@ -33,33 +34,47 @@ class Server
     static void HandleClient(TcpClient client, ConcurrentDictionary<TcpClient, Player> players)
     {
         Console.WriteLine("Connected to client " + ((IPEndPoint)client.Client.RemoteEndPoint).ToString());
-        players.TryAdd(client, new Player() {
-          Id="1", Name ="JOHNDOE", X=0, Y=0
+        players.TryAdd(client, new Player()
+        {
+            Id = "1",
+            Name = "JOHNDOE",
+            X = 0,
+            Y = 0
         });
         Console.WriteLine(PrettyPrint(players));
         NetworkStream stream = client.GetStream();
+        while (true)
+        {
+            try
+            {
+                byte[] buffer = new byte[1024];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
 
-        byte[] buffer = new byte[1024];
-        int bytesRead = stream.Read(buffer, 0, buffer.Length);
-        
-        string data = Encoding.ASCII.GetString(buffer, 0, bytesRead);
-        Console.WriteLine("Received message: " + data);
+                string data = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                Console.WriteLine("Received message: " + data);
 
-        string response = "Server response: " + data;
-        byte[] responseBuffer = Encoding.ASCII.GetBytes(response);
-        
-        stream.Write(responseBuffer, 0, responseBuffer.Length);
-        Console.WriteLine("Response sent.");
+                string response = "Server response: " + data;
+                byte[] responseBuffer = Encoding.ASCII.GetBytes(response);
 
-        stream.Close();
-        client.Close();
-        
+                stream.Write(responseBuffer, 0, responseBuffer.Length);
+                Console.WriteLine("Response sent.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                stream.Close();
+                client.Close();
+            }
+
+        }
+
     }
 
-    public static string PrettyPrint(Object o) {
-      var jsonString = JsonConvert.SerializeObject(
-           o, Formatting.Indented,
-           new JsonConverter[] {new StringEnumConverter()});
-      return jsonString;
+    public static string PrettyPrint(Object o)
+    {
+        var jsonString = JsonConvert.SerializeObject(
+             o, Formatting.Indented,
+             new JsonConverter[] { new StringEnumConverter() });
+        return jsonString;
     }
 }
